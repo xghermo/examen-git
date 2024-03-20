d8886b61a49c62127f6b98bb8f03231a5444d466
xabi <x.gonzalez.hermo@gmail.com>
2024-03-20 12:56:53 +0100

    CSS en mantenimiento

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
     <title>Examen Notas-Git</title>
 </head>
 <body>
@@ -13,7 +14,9 @@
     <form action="notas.php" method="POST">
         <input type="text" name="nombre" id="nombre" placeholder="Nombre :)" required>
         <br>
+        <br>
         <textarea name="texto" id="texto" cols="50" rows="5" placeholder="Escribe aquí tus experiencias, anecdotas o lo que te apetezca compartir con esta nuestra comunidad (L)(L)" required></textarea>
+        <br>
         <br>
         <button>COMPARTIR! XD</button>
     </form>
