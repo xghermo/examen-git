b50c5039c6ca4b88610c5537e7239c0c8489b555
xabi <x.gonzalez.hermo@gmail.com>
2024-03-21 11:01:19 +0100

    FINAL. El definitivo con AJAX

--- index.php
+++ index.php
@@ -3,14 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
     <title>Examen Notas-Git</title>
 </head>
 <body>
     <h1>COMUNIDAD BLOGGER</h1>
 
-    <h2>¡¡¡Bienvenid@s a la mayor comunidad blogger de todo el localhost!!! XD</h2>
-
     <form action="notas.php" method="POST">
+        <h2>¡¡¡Bienvenid@s a la mayor comunidad blogger de todo el localhost!!! XD</h2>
+
         <input type="text" name="nombre" id="nombre" placeholder="Nombre :)" required>
         <br>
         <textarea name="texto" id="texto" cols="50" rows="5" placeholder="Escribe aquí tus experiencias, anecdotas o lo que te apetezca compartir con esta nuestra comunidad (L)(L)" required></textarea>
@@ -20,6 +21,15 @@
 
     <section id="notas" name="notas">
         <h3>Notas compartidas:</h3>
+        <?php
+        $archivo = 'notas.txt';
+        if (file_exists($archivo)) {
+            $contenido = file_get_contents($archivo);
+            echo nl2br($contenido);
+        } else {
+            echo "No hay notas guardadas.";
+        }
+        ?>
     </section>
 
         <script>
@@ -61,19 +71,5 @@
 
         </script>
 
-
-        <?php
-        $archivo = 'notas.txt';
-        if (file_exists($archivo)) {
-            // Lee el contenido del archivo y lo muestra en el <section>
-            $contenido = file_get_contents($archivo);
-            echo nl2br($contenido); // nl2br() convierte saltos de línea en <br>
-        } else {
-            echo "No hay notas guardadas.";
-        }
-        ?>
-
-    
-    
 </body>
 </html>
